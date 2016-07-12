ed80e678d9422ace6ce9ebddf08b872528b5be86
Marie <marizamj@gmail.com>
2016-07-12 21:35:21 +0200

    Remove second canvas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 var XOGame = require('./XOGame');
 var XOGameView = require('./XOGameView');
 
-var game1 = new XOGame(4);
-var view1 = new XOGameView(game1, document.querySelector('#canvas1'));
-view1.setupEvents();
-
-var game2 = new XOGame(3);
-var view2 = new XOGameView(game2, document.querySelector('#canvas2'));
-view2.setupEvents();
+var game = new XOGame(4);
+var view = new XOGameView(game, document.querySelector('#canvas'));
+view.setupEvents();
